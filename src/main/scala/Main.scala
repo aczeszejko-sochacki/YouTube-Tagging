ec7a45b45b00a7b4f8b99a5f343ccbcc5aef6272
aczeszejko-sochacki <aleksanderczeso@gmail.com>
2020-05-25 13:37:11 +0200

    Alter passing files as arguments. Add throttling for article requests

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 package main
 
+import scala.concurrent.duration._
 import java.nio.file.Paths
 import scala.io.StdIn.readLine
 
@@ -28,29 +29,24 @@ object Main extends App with CaptionFlows with MainFlows {
   implicit val lang: Language = config.getString("captionsLanguage")
   val maxOpenRequests = config.getInt("akka.http.host-connection-pool.max-open-requests")
   implicit val parallelism: Parallelism = config.getInt("parallelism")
+  val articleRequestsPerSec = config.getInt("articleRequestsPerSec")
 
   require(parallelism * parallelism <= maxOpenRequests, "Wrong parellelism + max-open-requests values")
 
   def termination = { Http().shutdownAllConnectionPools; actorSystem.terminate }
 
   try {
-    println("Provide the source path (skip to set src/main/resources/exampleIds.csv)")
-    val sourcePath = readLine match {
-      case ""   => "src/main/resources/exampleIds.csv"
-      case path => path
-    }
-    println("Provide the name of the destination file (destination dir is src/main/resources))")
-    val (saveDir, destinationFilename) = ("src/main/resources/", readLine)
+    val (sourcePath, destPath) = (args(0), args(1))
 
     IdProvider.sourceFromPath(sourcePath)
       .via(idToYtIdCaptionsRaw)
       .via(ytCaptionsRawToParsed)
-      .map(x => { println(x.parsedCaptions); x })
       .via(ytCaptionsParsedtoTaggedVideo)
+      .throttle(articleRequestsPerSec, 1.second)
       .via(ytTaggedVideoToYtVideoArticle)
       .via(stringToJsonString)
       .map(ByteString(_))
-      .runWith(FileIO.toPath(Paths.get(saveDir + destinationFilename)))
+      .runWith(FileIO.toPath(Paths.get(destPath)))
       .foreach(_ => { log.info("Successfully downloaded the entire content"); termination })
   } catch {
     case _: WrongPathException => log.error("Wrong source path. Actor system terminated."); termination
